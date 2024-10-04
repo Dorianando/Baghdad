fbfe757b1a40b2f534ca16719fbe365efd1de8b0
Dorianando <doriansgolubjevs@gmail.com>
2024-10-04 11:35:09 +0300

    Kerbela

---
+++ infene.py
@@ -0,0 +1,18 @@
+if 8 < 22:
+    print ("8 < 22")
+    a = 8
+    b = 26
+    print (a + b)
+    print (a - b)
+    print (a * b)
+    print (a / b)
+    print (a % b)
+    
+    
+    a= int(input ("enter number a:"))
+   
+    print (a + b)
+    print (a - b)
+    print (a * b)
+    print (a / b)
+    print (a % b)
