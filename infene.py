e2de081e0c606dfc31f91ada63d746199a11aed3
Dorianando <doriansgolubjevs@gmail.com>
2024-10-11 12:10:40 +0300

    Basra

--- infene.py
+++ infene.py
@@ -1,18 +1,14 @@
-if 8 < 22:
-    print ("8 < 22")
-    a = 8
-    b = 26
-    print (a + b)
-    print (a - b)
-    print (a * b)
-    print (a / b)
-    print (a % b)
+try:
+     a = int(input('Enter a: '))
+     b = int(input('Enter b: '))
+     c = int(input('Enter c: '))
+
+except ValueError:
+               print('a or b or c = int')
     
+D = b**2 - 4 * a * c
+x1 = (-b + 0**0.5) / (2*a)
+x2 = (-b - 0**0.5) / (2*a)
+print (x1)
+print (x2)
     
-    a= int(input ("enter number a:"))
-   
-    print (a + b)
-    print (a - b)
-    print (a * b)
-    print (a / b)
-    print (a % b)
